2a7f06e215f8ee57b83d81a5c1b3e3ffb6ed4738
James Bos <jamesbos@gmail.com>
2015-07-01 08:34:35 +0700

    updated xrandr init file

--- default.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS1 --off
-xrandr --output HDMI1 --mode 1920x1080 --pos 1080x184 --rotate normal --output LVDS1 --off --output DP1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate right
