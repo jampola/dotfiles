50c56fab88dfad9e5172cc45705e0a2c77549b8b
james <james@delorean.localdomain>
2016-04-09 13:21:27 +0700

    ch ch ch chaaaangeeess

---
+++ xrandrsetup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DVI-D-0 --off --output DVI-D-1 --mode 1920x1080 --pos 0x112 --rotate right --output HDMI-0 --mode 3840x2160 --pos 1080x0 --rotate normal
